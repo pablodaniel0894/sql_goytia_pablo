b19817d3e29914b6a6c11e777295a12068c5e0d1
pablodaniel0894 <pgoytiagcba@gmail.com>
2025-04-04 12:21:42 -0300

    modificado el archivo de sql

--- test_sql.sql
+++ test_sql.sql
@@ -1,2 +1,5 @@
+-- Problemática detectada: Un comercio barrial quiere empezar a incursionar en el mundo de los pedidos online. Para ello, necesita establecer ciertas bases de datos para llevarlo a cabo lo mas eficiente posible
+
+
 CREATE DATABASE pedidos_online;
 USE pedidos_online;
